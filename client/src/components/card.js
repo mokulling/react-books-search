925f4427cfb756391926d692e54f5315b79cdda4
mokulling <72229978+mokulling@users.noreply.github.com>
2021-04-13 18:33:48 -0500

    set up route

---
+++ client/src/components/card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Card = ({title, img, authors, link, dataid, desc}) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <img src={img} className="img-fluid img" alt={title}/>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">Authors: {authors}</p>
+        <p className="card-text">Description: {desc}</p>
+        <a href={link} target="_blank" rel="noreferrer" className="btn btn-primary">Link to buy</a>
+      </div>
+    </div>
+  )
+}
+
+export default Card
